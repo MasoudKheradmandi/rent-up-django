3481794b413d9e0aa001b2421bb5d0d7941fabba
erfan--karimi <h410mi3@gmail.com>
2022-07-20 18:20:21 +0430

    detail agent be compelete and favlist

--- admin_informations/urls.py
+++ admin_informations/urls.py
@@ -5,4 +5,5 @@ app_name = 'admin_informations'
 
 urlpatterns = [
     path('listagent',views.agent_list,name='listagent'),
+    path('detailagent/<int:id>',views.agent_detail,name='detailagent'),
 ]
--- admin_informations/views.py
+++ admin_informations/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
-
+from .models import AdminInformation
 # Create your views here.
 
 
 def agent_list(request):
-    return render(request,'agents.html',{})
+    agents = AdminInformation.objects.all()
+    context = {
+        "agents" : agents ,
+    }
+    return render(request,'agents.html',context)
     
+def agent_detail(request,id):
+    agent = AdminInformation.objects.get(id=id)
+    context = {
+        'agent' : agent,
+    }
+    return render(request,'agent-page.html',context)
